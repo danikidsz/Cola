84e66d46a831564959f1b143f101b7f480433a5b
danikidsz <danit2ks@gmail.com>
2024-02-14 19:54:56 -0500

    Initial commit

---
+++ src/main/java/cola/Base.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cola;
+
+/**
+ *
+ * @author juan_dan.cardenas
+ */
+public abstract class Base {
+    
+    public abstract Base copy();
+}
